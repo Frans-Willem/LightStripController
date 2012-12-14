76c44a0bedb646977ff8bcdaa92198b16fa9730e
Frans-Willem Hardijzer <fw@hardijzer.nl>
2012-12-14 00:05:38 +0000

    - Updated RPi config - Custom baudrate sucks, but RPi seems to have 1000000 defined :)

--- outputs/serial/src/CSerialOutput.cpp
+++ outputs/serial/src/CSerialOutput.cpp
@@ -4,10 +4,11 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <termios.h>
-#include <linux/serial_core.h>
+/*#include <linux/serial_core.h>
 #include <asm/ioctls.h>
-#include <sys/ioctl.h>
-#include <termios.h>
+#include <sys/ioctl.h>*/
+
+#define BAUDCASE(x) case x: speed = B ## x ; break;
 
 CSerialOutput::CSerialOutput(int nFD) : m_nFD(nFD) {
 
@@ -19,7 +20,6 @@ CSerialOutput::~CSerialOutput() {
 
 void CSerialOutput::Write(const void *pData, size_t nLen) {
 	write(m_nFD, pData, nLen);
-	tcdrain(m_nFD);
 }
 
 CSerialOutput *CSerialOutput::Create(const char *szDevice, unsigned int baudrate) {
@@ -39,26 +39,54 @@ CSerialOutput *CSerialOutput::Create(const char *szDevice, unsigned int baudrate
 		close(nFD);
 		return NULL;
 	}
-	/*config.c_oflag = 0;
-	config.c_lflag &= ~(ECHO | ECHONL | ICANON | IEXTEN | ISIG);
-	config.c_cflag &= ~(CSIZE | PARENB | CSTOPB | CREAD | PARENB);
-	config.c_cflag |= CS8;*/
+
 	cfmakeraw(&config);
-	cfsetospeed(&config,B38400);
+	speed_t speed;
+	switch (baudrate) {
+		default:
+			fprintf(stderr, "%d is an unsupported baudrate.\n", baudrate);
+			close(nFD);
+			return NULL;
+		BAUDCASE(50)
+		BAUDCASE(75)
+		BAUDCASE(110)
+		BAUDCASE(134)
+		BAUDCASE(150)
+		BAUDCASE(200)
+		BAUDCASE(300)
+		BAUDCASE(600)
+		BAUDCASE(1200)
+		BAUDCASE(1800)
+		BAUDCASE(2400)
+		BAUDCASE(4800)
+		BAUDCASE(9600)
+		BAUDCASE(19200)
+		BAUDCASE(38400)
+		BAUDCASE(57600)
+		BAUDCASE(115200)
+		BAUDCASE(230400)
+		BAUDCASE(460800)
+		BAUDCASE(500000)
+		BAUDCASE(576000)
+		BAUDCASE(921600)
+		BAUDCASE(1000000)
+		BAUDCASE(1152000)
+		BAUDCASE(1500000)
+		BAUDCASE(2000000)
+		BAUDCASE(2500000)
+		BAUDCASE(3000000)
+		BAUDCASE(3500000)
+		BAUDCASE(4000000)
+	}
+	cfsetospeed(&config, speed);
+	cfsetispeed(&config, speed);
 	config.c_cflag &= ~(CREAD);
-	if (tcsetattr(nFD, TCSAFLUSH, &config) < 0) {
+
+	if (tcsetattr(nFD, TCSANOW, &config) < 0) {
 		fprintf(stderr, "'%s' is not a valid serial device (tcsetattr failed).\n", szDevice);
 		close(nFD);
 		return NULL;
 	}
-	struct serial_struct ss;
-	ioctl(nFD, TIOCGSERIAL, &ss);
-	ss.flags = (ss.flags & ~ASYNC_SPD_MASK) | ASYNC_SPD_CUST;
-	//ss.baud_base = 16000000;
-	ss.custom_divisor = (ss.baud_base + (baudrate / 2)) / baudrate;
-	if (ss.custom_divisor < 1)
-		ss.custom_divisor = 1;
-	printf("baud_base: %d %d %d\n", ss.baud_base, ss.custom_divisor, ss.baud_base / ss.custom_divisor);
-	ioctl(nFD, TIOCSSERIAL, &ss);
+	tcflush(nFD, TCIOFLUSH);
 	return new CSerialOutput(nFD);
 }
